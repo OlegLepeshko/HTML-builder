82145c9f62762aa2ed9595df6ebbcea6de9efd63
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2022-05-23 21:13:07 +0300

    feat: add four solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+const analogFolder = async () => {
+  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+  fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+    files.forEach(async (file) => {
+      await fs.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  });
+};
+
+analogFolder();
